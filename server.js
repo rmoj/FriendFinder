6094bd7a286e800cc13aaf4f071f7787e6ce11e0
rmoj <rmoj99@gmail.com>
2018-09-13 14:51:41 -0400

    Fixed: server not starting

--- server.js
+++ server.js
@@ -6,12 +6,12 @@ var app = express();
 
 var PORT = process.env.PORT || 8888;
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+var jsonParser = bodyParser.json();
 
-app.use(bodyParser, json({ type: 'application/*+json' }));
-app.use(bodyParser, raw({ type: 'application/vnd.custom-type' }));
-app.use(bodyParser, text({ type: 'text/html' }));
+app.use(bodyParser.json({ type: 'application/*+json' }));
+app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }));
+app.use(bodyParser.text({ type: 'text/html' }));
 
 app.listen(PORT, function() {
   console.log('App listening on PORT: ' + PORT);
