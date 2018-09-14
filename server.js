20185356693ae1374dd0922c41481a6339fabdb5
rmoj <rmoj99@gmail.com>
2018-09-13 22:47:55 -0400

    Updated: server.js - added api route

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }));
 app.use(bodyParser.text({ type: 'text/html' }));
 
 require('./app/routing/htmlRoutes.js')(app);
-// require('./app/routing/apiRoutes.js')(app);
+require('./app/routing/apiRoutes.js')(app);
 
 app.listen(PORT, function() {
   console.log('App listening on PORT: ' + PORT);
