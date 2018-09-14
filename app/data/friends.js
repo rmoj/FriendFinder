25b3aa2971b45b0c8e18038b5c79fdfad6990932
rmoj <rmoj99@gmail.com>
2018-09-13 22:37:03 -0400

    Added: friends.js

---
+++ app/data/friends.js
@@ -0,0 +1,22 @@
+var friends = [
+  {
+    name: 'Emma',
+    photo: './images/emma.jpg',
+    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+  },
+  {
+    name: 'Olivia',
+    photo: './images/olivia.jpg',
+    scores: [4, 5, 3, 3, 4, 5, 1, 4, 3, 5]
+  },
+  {
+    name: 'Sophia',
+    photo: './images/sophia.jpg',
+    scores: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+  },
+  {
+    name: 'Isabella',
+    photo: './images/isabella.jpg',
+    scores: [2, 4, 2, 4, 2, 4, 2, 4, 2, 4]
+  }
+];
