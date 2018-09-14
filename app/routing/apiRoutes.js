d19564c8d48df8f3b8d2ca6d3d0c8fe265fe689d
rmoj <rmoj99@gmail.com>
2018-09-14 02:01:46 -0400

    Updated: apiRoute.js -  added functionality to compute compatibility

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,4 +4,38 @@ module.exports = function(app) {
   app.get('/api/friends', function(req, res) {
     res.json(friends);
   });
+
+  app.post('/api/friends', function(req, res) {
+    var bestMatch = {
+      name: '',
+      photo: '',
+      difference: 100
+    };
+
+    var user = req.body;
+    var userScores = user.scores;
+
+    var totalDifference = 0;
+
+    friends.forEach(function(friend) {
+      totalDifference = 0;
+      var friendScores = friend.scores;
+
+      for (var i = 0; i < friendScores.length; i++) {
+        totalDifference += Math.abs(
+          parseInt(userScores[i]) - parseInt(friendScores[i])
+        );
+      }
+
+      if (totalDifference <= bestMatch.difference) {
+        bestMatch.name = friend.name;
+        bestMatch.photo = friend.photo;
+        bestMatch.difference = totalDifference;
+      }
+
+      friends.push(user);
+
+      res.json(bestMatch);
+    });
+  });
 };
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -5,9 +5,6 @@ module.exports = function(app) {
     res.sendFile(path.join(__dirname + '/../public/survey.html'));
   });
 
-  // app.use(function(req, res) {
-  //   res.sendFile(path.join(__dirname + '/../public/home.html'));
-  // });
   app.get('*', function(req, res) {
     res.sendFile(path.join(__dirname + '/../public/home.html'));
   });
