20c3c486d8507d92b5b5c3a37d52c1ca61a6de42
rmoj <rmoj99@gmail.com>
2018-09-14 00:48:19 -0400

    Updated: links to profile images

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,22 +1,22 @@
 var friends = [
   {
     name: 'Emma',
-    photo: './images/emma.jpg',
+    photo: 'https://source.unsplash.com/K_b41GaWC5Y',
     scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
   },
   {
     name: 'Olivia',
-    photo: './images/olivia.jpg',
+    photo: 'https://source.unsplash.com/D929R3i3Ai4',
     scores: [4, 5, 3, 3, 4, 5, 1, 4, 3, 5]
   },
   {
     name: 'Sophia',
-    photo: './images/sophia.jpg',
+    photo: 'https://source.unsplash.com/J2rmlSiJl2I',
     scores: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
   },
   {
     name: 'Isabella',
-    photo: './images/isabella.jpg',
+    photo: 'https://source.unsplash.com/9wVhqVa9H38',
     scores: [2, 4, 2, 4, 2, 4, 2, 4, 2, 4]
   }
 ];
